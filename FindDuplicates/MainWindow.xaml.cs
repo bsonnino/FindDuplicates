ee8f4eadca8983e96d949fd253e2be2a52d22e1f
Cees Kaas <cees.kaas@gmail.com>
2021-04-17 14:12:06 +0200

    Made it optionally find all files that have duplicates instead of only the top 100, added option to only compare the first 10kbytes, added remove button per file.

--- FindDuplicates/MainWindow.xaml.cs
+++ FindDuplicates/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using WPFFolderBrowser;
 
 namespace FindDuplicates
@@ -47,7 +48,7 @@ namespace FindDuplicates
             return files;
         }
 
-        private async Task<List<IGrouping<long, FileInfo>>> GetPossibleDuplicatesAsync(string selectedPath)
+        private async Task<List<IGrouping<long, FileInfo>>> GetPossibleDuplicatesAsync(string selectedPath, bool takeSmallResultSet)
         {
             List<IGrouping<long, FileInfo>> files = null;
             await Task.Factory.StartNew(() =>
@@ -56,7 +57,7 @@ namespace FindDuplicates
                                .OrderByDescending(f => f.Length)
                                  .GroupBy(f => f.Length)
                                  .Where(g => g.Count() > 1)
-                                 .Take(100)
+                                 .Take(takeSmallResultSet ? 100 : int.MaxValue)
                                  .ToList();
             });
             return files;
@@ -64,6 +65,9 @@ namespace FindDuplicates
 
         private async void StartClick(object sender, RoutedEventArgs e)
         {
+            var takeSmallResultSet = ChkTakeSmallResultSet.IsChecked ?? false;
+            var compareOnlyFirstPartOfFile = ChkCompareOnlyFirstPartOfFile.IsChecked ?? false;
+
             var fbd = new WPFFolderBrowserDialog();
             if (fbd.ShowDialog() != true)
                 return;
@@ -72,8 +76,8 @@ namespace FindDuplicates
             FilesList.ItemsSource = null;
             var selectedPath = fbd.FileName;
 
-            var files = await GetPossibleDuplicatesAsync(selectedPath);
-            var realDuplicates = await GetRealDuplicatesAsync(files);
+            var files = await GetPossibleDuplicatesAsync(selectedPath, takeSmallResultSet);
+            var realDuplicates = await GetRealDuplicatesAsync(files, compareOnlyFirstPartOfFile);
             FilesList.ItemsSource = realDuplicates;
             sw.Stop();
             var allFiles = realDuplicates.SelectMany(f => f.Value).ToList();
@@ -81,15 +85,15 @@ namespace FindDuplicates
                 $"({allFiles.Sum(f => f.Length):N0} total duplicate bytes) {sw.ElapsedMilliseconds} ms";
         }
 
-        private static async Task<Dictionary<string, List<FileInfo>>> GetRealDuplicatesAsync(
-            List<IGrouping<long, FileInfo>> files)
+        private async Task<Dictionary<string, List<FileInfo>>> GetRealDuplicatesAsync(
+            List<IGrouping<long, FileInfo>> files, bool compareOnlyFirstPartOfFile)
         {
             var dictFiles = new ConcurrentDictionary<string, ConcurrentBag<FileInfo>>();
             await Task.Factory.StartNew(() =>
             {
                 Parallel.ForEach(files.SelectMany(g => g), file =>
                     {
-                        var hash = GetMD5HashFromFile(file.FullName);
+                        var hash = GetMD5HashFromFile(file.FullName, compareOnlyFirstPartOfFile);
                         dictFiles.AddOrUpdate(hash, key => new ConcurrentBag<FileInfo>(new List<FileInfo> { file }),
                             (s, bag) =>
                             {
@@ -103,14 +107,25 @@ namespace FindDuplicates
                 .ToDictionary(p => p.Key, p => p.Value.ToList());
         }
 
-        public static string GetMD5HashFromFile(string fileName)
+        public string GetMD5HashFromFile(string fileName, bool CompareOnlyFirstPartOfFile)
         {
             try
             {
                 using (FileStream file = new FileStream(fileName, FileMode.Open))
                 {
                     var md5 = new MD5CryptoServiceProvider();
-                    var retVal = md5.ComputeHash(file);
+                    byte[] retVal;
+                    if (CompareOnlyFirstPartOfFile)
+                    {
+                        var bytes = new byte[10000];
+                        var readBytes = file.Read(bytes, 0, 10000);
+                        retVal = md5.ComputeHash(bytes, 0, readBytes);
+                    }
+                    else
+                    {
+                        retVal = md5.ComputeHash(file);
+                    }
+
                     file.Close();
                     return BitConverter.ToString(retVal);
                 }
@@ -118,6 +133,31 @@ namespace FindDuplicates
             catch (Exception ex) when (ex is UnauthorizedAccessException || ex is IOException)
             {
                 return null;
+            }
+        }
+
+        private void RemoveFile(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            if (button == null) return;
+
+            if (button.DataContext is FileInfo file && (!(ChkCheckBeforeDelete.IsChecked ?? true) || MessageBox.Show($"Are you sure you want to delete {file.FullName}?", "SURE?", MessageBoxButton.YesNo,
+                MessageBoxImage.Question, MessageBoxResult.No) == MessageBoxResult.Yes))
+            {
+                var compareOnlyFirstPartOfFile = ChkCompareOnlyFirstPartOfFile.IsChecked ?? false;
+                var hash = GetMD5HashFromFile(file.FullName, compareOnlyFirstPartOfFile);
+                file.Delete();
+                var filesListItemsSource = (Dictionary<string, List<FileInfo>>)FilesList.ItemsSource;
+                if (filesListItemsSource.TryGetValue(hash, out var item))
+                {
+                    FilesList.ItemsSource = null;
+                    item.Remove(file);
+                    if (item.Count == 1)
+                    {
+                        filesListItemsSource.Remove(hash);
+                    }
+                    FilesList.ItemsSource = filesListItemsSource;
+                }
             }
         }
     }
